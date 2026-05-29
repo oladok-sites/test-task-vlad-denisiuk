d790894377dbcc0ca38149b97bcca21c0c390f4f
oladok-sites <denysiukvladyslavivan@gmail.com>
2026-05-29 16:29:56 +0300

    feat: fixed bugs with admin panel and added it to burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,6 +59,14 @@ export default function Header() {
 						>
 							О нас
 						</Link>
+                        <Link
+                            href='/admin'
+                            className={`text-sm font-medium transition-colors ${
+                                isActive('/admin') || isActive('/admin-login') ? 'text-white' : 'text-slate-400 hover:text-white'
+                            }`}
+                        >
+                            Админ-панель
+                        </Link>
 						<Link
 							href='/contact'
 							className={`text-sm font-medium transition-colors ${
